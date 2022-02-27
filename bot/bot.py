67bb0cecc1783afe1a52a21191273378b31d786a
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-02-27 11:09:54 -0500

    transfer to disnake

--- bot/bot.py
+++ bot/bot.py
@@ -1,10 +1,8 @@
 """Main file where the bot runs from and loads extensions."""
-from discord.ext import commands
+from disnake.ext import commands
 from config import TOKEN
 
 
 bot = commands.Bot(command_prefix="~", case_insensitive=True)
-
-bot.load_extension("cogs.leagues")
 
 bot.run(TOKEN)
